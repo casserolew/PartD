fa5dc7aafd202bc73cebba1f1fb10b07273365ad
Cassidy Wenmoth <c.wenmoth@gmail.com>
2020-08-28 16:52:07 +1200

    Commit 2.

--- Part D/src/PartD.java
+++ Part D/src/PartD.java
@@ -2,6 +2,6 @@
 public class PartD {
 	Hi, my name is Cassidy.
 	My favourite colour is Blue.
-	My favourite mode of transportation is
-	The best day of the week is
+	My favourite mode of transportation is boat.
+	The best day of the week is 
 }
